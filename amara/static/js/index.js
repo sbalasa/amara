afd2885a78efd83e4fd81a65df5d5def2b68af6b
Santhosh Balasa <santhosh.kbr@gmail.com>
2021-06-13 10:48:48 +0000

    Using Axios for Ajax calls

--- amara/static/js/index.js
+++ amara/static/js/index.js
@@ -1,41 +1,38 @@
 function execute_ajax() {
-    let ajax_response = false;
-    var token = document.getElementsByName("csrfToken").value;
-    $.ajax({
-        type: "POST",
-        url: "/ajax_form",
-        contentType: 'application/json',
-        headers: {
-            'X-CSRF-Token': token 
-        },
-        data: JSON.stringify({
-            name: document.querySelector("#name").value,
-            email: document.querySelector("#email").value,
-            subscriptions: document.querySelector("#input_subscriptions").value
-        }),
-        success: function(res) {
-            let complete_subscriptions = JSON.parse("{{ complete_subscriptions | escapejs }}");
-            let table_data = document.getElementById("table_data");
-            for (let i = 0; i < complete_subscriptions.length; i++) {
-                let row = document.createElement("TR");
-                row.className = "item";
-                let name = document.createElement("TD");
-                let email = document.createElement("TD");
-                let subscriptions = document.createElement("TD");
-                name.appendChild(document.createTextNode(complete_subscriptions[i].fields.name));
-                email.appendChild(document.createTextNode(complete_subscriptions[i].fields.email));
-                subscriptions.appendChild(document.createTextNode(complete_subscriptions[i].fields.subscriptions));
-                row.appendChild(name);
-                row.appendChild(email);
-                row.appendChild(subscriptions);
-                table_data.appendChild(row);
-            }
-            ajax_response = true;
-        },
-        error: function() {
-            console.log("Ajax Failed");
-        },
-        async: false
+
+    body = {
+        name: "Santhohs",
+        email: "santhosh.kbr@gmail.com"
+    } ;
+    axios
+    .post("/ajax_form", body)
+    .then((response) => {
+        console.log(response);
+    }, (error) => {
+        console.log(error);
     });
-    return ajax_response;
+
+    // axios.post("/ajax_form", body, { headers: {
+    //     'Content-Type': 'json'
+    // }}).then(response => {
+    //     console.log("Ajax Successful")
+    //     let table_data = document.getElementById("table_data");
+    //     for (let i = 0; i < complete_subscriptions.length; i++) {
+    //         let row = document.createElement("TR");
+    //         row.className = "item";
+    //         let name = document.createElement("TD");
+    //         let email = document.createElement("TD");
+    //         let subscriptions = document.createElement("TD");
+    //         name.appendChild(document.createTextNode(complete_subscriptions[i].fields.name));
+    //         email.appendChild(document.createTextNode(complete_subscriptions[i].fields.email));
+    //         subscriptions.appendChild(document.createTextNode(complete_subscriptions[i].fields.subscriptions));
+    //         row.appendChild(name);
+    //         row.appendChild(email);
+    //         row.appendChild(subscriptions);
+    //         table_data.appendChild(row);
+    //     }
+    // }).catch(function(error) {
+    //     console.log("Ajax failed");
+    //     console.log(error);
+    // });
 }
